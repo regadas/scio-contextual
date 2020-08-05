8ce0dc7cb7c90ef4af83b082e147c8f6df0add02
Scala Steward <me@scala-steward.org>
2020-08-06 00:41:14 +0200

    Update scio-bigquery to 0.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val Contextual = "com.propensive" %% "contextual" % "1.2.1"
-  lazy val ScioBigQuery = "com.spotify" %% "scio-bigquery" % "0.9.2"
+  lazy val ScioBigQuery = "com.spotify" %% "scio-bigquery" % "0.9.3"
   lazy val MUnit = "org.scalameta" %% "munit" % "0.7.10"
 }
