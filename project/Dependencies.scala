bd43c5ed13794fc90398bf117a1a30304445d8cf
Scala Steward <me@scala-steward.org>
2020-11-01 12:54:11 +0100

    Update munit to 0.7.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val Contextual = "com.propensive" %% "contextual" % "1.2.1"
   lazy val ScioBigQuery = "com.spotify" %% "scio-bigquery" % "0.9.4"
-  lazy val MUnit = "org.scalameta" %% "munit" % "0.7.14"
+  lazy val MUnit = "org.scalameta" %% "munit" % "0.7.15"
 }
